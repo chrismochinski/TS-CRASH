f750cc7cd120c36b21cd6a01a3b6e6647d330a9b
Chris Mochinski <cmochinski@gmail.com>
2022-02-13 16:05:26 -0600

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+let id = 5;
+//inference = knows it's a number even if you don't specify
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+let id: number = 5;
+//inference = knows it's a number even if you don't specify
