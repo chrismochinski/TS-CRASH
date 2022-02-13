6ccfb67d5510a36f7a4af70df8579dc5df4c4787
Chris Mochinski <cmochinski@gmail.com>
2022-02-13 16:14:37 -0600

    inference, initialization, terms

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,11 @@
 "use strict";
+//basic types:
 let id = 5;
+let company = "Holy Moses Media";
+let isPublished = true;
+let x = "hello!";
+console.log('id is: ', id);
+x = 4; //able to change as "any" 
+let age; //initialize but not declare!
+age = 40;
 //inference = knows it's a number even if you don't specify
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
+//basic types:
+
 let id: number = 5;
+let company: string = "Holy Moses Media";
+let isPublished: boolean = true;
+let x: any = "hello!";
+
+console.log('id is: ', id)
+
+
+x = 4; //able to change as "any" 
+
+
+let age: number; //initialize but not declare!
+
+age = 40;
+
 //inference = knows it's a number even if you don't specify
